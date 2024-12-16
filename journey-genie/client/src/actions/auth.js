2026a0336a503b3f1a250bd53b2b2a8034033810
Deepali Nagwade <nagwade.deepali@gmail.com>
2024-12-15 22:14:56 -0500

    login and logout

--- journey-genie/client/src/actions/auth.js
+++ journey-genie/client/src/actions/auth.js
@@ -60,3 +60,39 @@ export const register =
       });
     }
   };
+
+
+// Login User
+export const login = (email, password) => async (dispatch) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const body = JSON.stringify({ email, password });
+  
+    try {
+      const res = await axios.post('/api/auth', body, config);
+  
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+  
+      dispatch(loadUser());
+    } catch (err) {
+      const errors = err.response.data.errors;
+  
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+      dispatch({
+        type: LOGIN_FAIL,
+      });
+    }
+  };
+  
+  // Logout / Clear Profile
+  export const logout = () => (dispatch) => {
+    dispatch({ type: LOGOUT });
+  };
