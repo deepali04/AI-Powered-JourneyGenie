53a87b4ee1e135bac9608b67cde28c7b2765dfd8
Deepali Nagwade <nagwade.deepali@gmail.com>
2024-12-15 22:11:17 -0500

    display alert

---
+++ journey-genie/client/src/actions/alerts.js
@@ -0,0 +1,24 @@
+import { SET_ALERT, REMOVE_ALERT } from "./types";
+import { v4 as uuidv4 } from "uuid";
+
+export const setAlert =
+  (msg, alertType, timeout = 5000) =>
+  (dispatch) => {
+    const id = uuidv4();
+    //   const id = Math.random().toString(36).substring(7);
+    
+
+    // Add class to container element
+    const container = document.querySelector(".container");
+    container.classList.add("alert-shown");
+
+    setTimeout(() => {
+      // Remove class after 5 seconds
+      container.classList.remove("alert-shown");
+
+      dispatch({
+        type: REMOVE_ALERT,
+        payload: id,
+      });
+    }, timeout);
+  };
